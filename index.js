829778823d445dc4057c4f3d5c20486b47ee16c8
Madona Wayne Ngobeni <madonawaynengobeni@gmail.com>
2024-09-12 19:38:02 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const years = document.getElementById('count-years');
-const months = document.getElementById('count-months');
-const days = document.getElementById('count-days');
+const inputYears = document.getElementById('year');
+const inputMonths = document.getElementById('month');
+const inputDays = document.getElementById('day');
+const button = document.getElementsByClassName('btn');
+
+/*button.addEventListener("click", function () {
+    const years = document.getElementById("count-years").innerText;
+    const months = document.getElementById("count-months").innerText;
+    const days = document.getElementById("count-days").innerText;
+    const date = new Date();
+    
+    years = date.getFullYear - inputYears;
+
+});*/
 
