24fca5280115c0fece2d508c1dde7736f0e4b203
Madona Wayne Ngobeni <madonawaynengobeni@gmail.com>
2024-10-08 21:54:15 +0200

    update

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const inputYears = document.getElementById('year');
-const inputMonths = document.getElementById('month');
-const inputDays = document.getElementById('day');
+/*const inputYears = document.getElementById('year').value;
+const inputMonths = document.getElementById('month').value;
+const inputDays = document.getElementById('day').value;
 const button = document.getElementsByClassName('btn');
+*/
 
-/*button.addEventListener("click", function () {
-    const years = document.getElementById("count-years").innerText;
-    const months = document.getElementById("count-months").innerText;
-    const days = document.getElementById("count-days").innerText;
-    const date = new Date();
-    
-    years = date.getFullYear - inputYears;
 
-});*/
+
+$(".btn").on("click", function() {
+    const dat = new Date();
+    let month = dat.getMonth();
+    let year = dat.getFullYear();
+    let day = dat.getDate();
+
+
+    let inputYear = document.getElementById("year").value;
+    let inputMonth = document.getElementById("month").value;
+    let inputDays = document.getElementById("day").value;
+    if (inputDays.length > 2 ) {
+        alert("Enter number");
+    } else if (inputMonth.length > 2) {
+        return "wrong input";
+    } else if (inputYear.length > 4 || inputYear.length < 4) {
+        return "wrong input";
+    }
+        let resultYear = year - inputYear;
+        let resultMonth = month - inputMonth;
+        let resultDay = day - inputDays;
+        $("#count-years").text(`${resultYear}`);
+        $("#count-months").text(`${resultMonth}`);
+        $("#count-days").text(`${resultDay}`);
+});
+   
 
