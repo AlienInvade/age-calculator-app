5dbcbf0a1e8d89295f8814c9fa44bbc3192f74b8
Madona Wayne Ngobeni <madonawaynengobeni@gmail.com>
2024-10-13 16:35:22 +0200

    update

--- index.js
+++ index.js
@@ -11,32 +11,37 @@ $("#form").on("submit", function(event) {
     let inputDays = document.getElementById("day").value;
     const form = document.getElementById("form");
     // validates inputs 
+
     if (inputDays.length > 2  || inputDays > 31) {
         $(".error-day").text("Must be a valid date").toggle();
         $("input").css("border-color", "red");
         $("input").css("border-width", "1px")
+        $(".heading").css("color", "red");
         event.preventDefault();
-    } else if (inputMonth.length > 2 || inputMonth > 12) {
+    } 
+    if (inputMonth.length > 2 || inputMonth > 12) {
         $(".error-month").text("Must be a valid month").toggle();
         $("input").css("border-color", "red");
         $("input").css("border-width", "1px")
         event.preventDefault();
-    } else if (inputYear.length > 4 || inputYear.length < 4) {
+    }
+    if (inputYear.length > 4 || inputYear.length < 4) {
         $(".error-year").text("Must be a valid month").toggle();
         $("input").css("border-color", "red");
         $("input").css("border-width", "1px")
         event.preventDefault();
-        if (inputYear > year) {
+    }
+    if (inputYear > year) {
         $(".error-year").text("Must be in the past").toggle();
         $("input").css("border-color", "red");
         $("input").css("border-width", "1px")
         event.preventDefault();
+        return;
         }
-    }
     // calculates how old you are by age, month and day
         let resultYear = year - inputYear;
         let resultMonth = month - inputMonth;
-        let resultDay = inputDays - day;
+        let resultDay = day - inputDays;
         $("#count-years").text(`${resultYear}`);
         $("#count-months").text(`${resultMonth}`);
         $("#count-days").text(`${resultDay}`);
